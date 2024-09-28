b69dc2787ef5b6bee948893fac58baf3e359b8c5
michellechang02 <changmi@seas.upenn.edu>
2024-09-28 11:41:04 -0400

    backend deploy

--- api/index.py
+++ api/index.py
@@ -1,8 +1,20 @@
+import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routes.routes import root
 
 ### Create FastAPI instance with custom docs and openapi url
-app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
+app = FastAPI()
 
-@app.get("/api/py/helloFastApi")
-def hello_fast_api():
-    return {"message": "Hello from FastAPI"}
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://veterans-society.vercel.app", "http://localhost:5173"], 
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
+# All routes 
+app.get("/")(root)
---
+++ api/routes/routes.py
@@ -0,0 +1,4 @@
+
+
+async def root():
+    return "Hello, World!"
