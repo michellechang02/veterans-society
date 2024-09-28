60490aca21ff20cf4b684cfa97726cc0ed1abac0
michellechang02 <changmi@seas.upenn.edu>
2024-09-28 11:30:38 -0400

    login + register in tsx

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,15 +1,77 @@
 import React from 'react';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Heading,
+  Stack,
+  Center,
+  Text,
+} from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
-interface LoginProps {
-  // Define props here if any exist
-}
+const Login: React.FC = () => {
+  const navigate = useNavigate();
 
-const Login: React.FC<LoginProps> = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Handle login logic here
+  };
+
   return (
-    <div>
-      {/* Add login content here */}
-    </div>
+    <Center h="100vh">
+      <Box 
+        p={10} 
+        width="30vw" 
+        minWidth="350px"
+        borderWidth={2} 
+        borderRadius={12} 
+        boxShadow="2xl"
+      >
+        <Heading mb={8} textAlign="center" fontSize="3xl">
+          Login
+        </Heading>
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={6}>
+            <FormControl id="email" isRequired>
+              <FormLabel fontSize="lg">Email</FormLabel>
+              <Input 
+                type="email" 
+                placeholder="Enter your email" 
+                size="lg"
+              />
+            </FormControl>
+
+            <FormControl id="password" isRequired>
+              <FormLabel fontSize="lg">Password</FormLabel>
+              <Input 
+                type="password" 
+                placeholder="Enter your password" 
+                size="lg"
+              />
+            </FormControl>
+
+            <Button 
+              bgColor="gray.500" 
+              color="white"
+              size="lg" 
+              type="submit" 
+              width="full"
+              fontSize="lg"
+              fontWeight="bold"
+            >
+              Login
+            </Button>
+          </Stack>
+        </form>
+        <Text mt={6} textAlign="center" fontSize="lg">
+          Don't have an account? <Button variant="link" colorScheme="gray" onClick={() => navigate('/register')}>Sign Up</Button>
+        </Text>
+      </Box>
+    </Center>
   );
-}
+};
 
 export default Login;
