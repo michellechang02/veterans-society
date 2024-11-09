8bae024b817117a0e40bedad6d25e0bcbf4eae4a
Matt Fu <mattfu25@gmail.com>
2024-11-09 14:14:19 -0500

    Format code

--- frontend/src/components/Feed.tsx
+++ frontend/src/components/Feed.tsx
@@ -1,24 +1,29 @@
-import { Box, Checkbox, Grid, Heading, Input, Text,
-    VStack, HStack, IconButton, Icon } from '@chakra-ui/react';
-import { Search } from 'react-feather'
-import Post from './Post';
-
+import {
+  Box,
+  Checkbox,
+  Grid,
+  Heading,
+  Input,
+  Text,
+  VStack,
+  HStack,
+  IconButton,
+  Icon,
+} from "@chakra-ui/react";
+import { Search } from "react-feather";
+import Post from "./Post";
 
 const Feed = () => {
-
-
-    const firstName = 'Michelle';
-    const lastName = 'Chang';
+  const firstName = "Michelle";
+  const lastName = "Chang";
 
   return (
-    <Grid
-      templateColumns="1fr 2fr 1fr"
-      gap={4}
-      p={4}
-    >
+    <Grid templateColumns="1fr 2fr 1fr" gap={4} p={4}>
       {/* Left Column: Search Filters */}
       <Box border="1px solid" borderColor="gray.200" borderRadius="md" p={4}>
-        <Heading as="h3" size="md" mb={4}>Search Filters</Heading>
+        <Heading as="h3" size="md" mb={4}>
+          Search Filters
+        </Heading>
         <VStack spacing={4} align="start">
           <Checkbox>Mental Health</Checkbox>
           <Checkbox>Employment</Checkbox>
@@ -28,32 +33,38 @@ const Feed = () => {
       </Box>
 
       {/* Middle Column: Input and Feed/Posts */}
-      <Box  pb={4} px={4} >
+      <Box pb={4} px={4}>
         <HStack mb={4}>
-        <Input placeholder="Search..."/>
-        <IconButton aria-label="Search" icon={<Icon as={Search} />} />
+          <Input placeholder="Search..." />
+          <IconButton aria-label="Search" icon={<Icon as={Search} />} />
         </HStack>
         <VStack spacing={4} align="stretch">
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
         </VStack>
       </Box>
 
       {/* Right Column: User Info and Goals */}
       <Box border="1px solid" borderColor="gray.200" borderRadius="md" p={4}>
-        <Text fontWeight="bold" fontSize="lg" mb={4}>Hi {firstName} {lastName}!</Text>
-        <Heading as="h4" size="md" mb={4}>Today's Goals</Heading>
+        <Text fontWeight="bold" fontSize="lg" mb={4}>
+          Hi {firstName} {lastName}!
+        </Text>
+        <Heading as="h4" size="md" mb={4}>
+          Today's Goals
+        </Heading>
 
         {/* Exercise Goals */}
-        <Text fontWeight="bold" mb={2}>Exercise:</Text>
+        <Text fontWeight="bold" mb={2}>
+          Exercise:
+        </Text>
         <VStack spacing={2} align="start">
           <Checkbox>goal 1</Checkbox>
           <Checkbox>goal 2</Checkbox>
@@ -63,7 +74,9 @@ const Feed = () => {
         </VStack>
 
         {/* Nutrition Goals */}
-        <Text fontWeight="bold" mt={4} mb={2}>Nutrition:</Text>
+        <Text fontWeight="bold" mt={4} mb={2}>
+          Nutrition:
+        </Text>
         <VStack spacing={2} align="start">
           <Checkbox>goal 1</Checkbox>
           <Checkbox>goal 2</Checkbox>
@@ -71,6 +84,6 @@ const Feed = () => {
       </Box>
     </Grid>
   );
-}
+};
 
 export default Feed;
--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,14 +1,14 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Text } from "@chakra-ui/react";
 
-type Props = {}
+type Props = {};
 
 function Post({}: Props) {
   return (
     <Box p={4} border="1px solid" borderColor="gray.200" borderRadius="md">
-    <Text fontWeight="bold">Post Title 1</Text>
-    <Text mt={2}>This is a description of the first post.</Text>
-  </Box>
-  )
+      <Text fontWeight="bold">Post Title 1</Text>
+      <Text mt={2}>This is a description of the first post.</Text>
+    </Box>
+  );
 }
 
-export default Post
+export default Post;
