e47a9fd9e9a0a62f87139bff544237a3975611d0
Matt Fu <mattfu25@gmail.com>
2025-01-19 12:20:20 -0500

    Add initial maps implementation

--- frontend/src/components/MapDisplay.tsx
+++ frontend/src/components/MapDisplay.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { VeteranResource } from '../Api/OverpassService';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 interface MapDisplayProps {
   resources: VeteranResource[];
@@ -9,8 +12,53 @@ interface MapDisplayProps {
   };
 }
 
-export const MapDisplay: React.FC<MapDisplayProps> = () => {
+// Fix for default marker icon
+const defaultIcon = L.icon({
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+L.Marker.prototype.options.icon = defaultIcon;
+
+export const MapDisplay: React.FC<MapDisplayProps> = ({ resources, userLocation }) => {
   return (
-    <div>Map Display Component - To be implemented in Phase 4</div>
+    <MapContainer
+      center={[userLocation.lat, userLocation.lon] as [number, number]}
+      zoom={13}
+      scrollWheelZoom={true}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        maxZoom={19}
+      />
+      
+      {/* User Location Marker */}
+      <Marker position={[userLocation.lat, userLocation.lon]}>
+        <Popup>Your Location</Popup>
+      </Marker>
+
+      {/* Resource Markers */}
+      {resources.map((resource) => (
+        <Marker
+          key={resource.id}
+          position={[resource.latitude, resource.longitude]}
+        >
+          <Popup>
+            <div>
+              <strong>{resource.name}</strong>
+              <br />
+              {resource.address}
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 };
