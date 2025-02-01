1c754bd9a7b17dae371d413815495422e83cc6b5
michellechang02 <changmi@seas.upenn.edu>
2025-02-01 08:03:25 -0500

    fix(fixed form): fix form frontend

--- frontend/src/components/Donate.tsx
+++ frontend/src/components/Donate.tsx
@@ -10,6 +10,9 @@ import {
     HStack,
     IconButton,
     Spacer,
+    VStack,
+    Heading,
+    useToast,
 } from '@chakra-ui/react';
 import { BiArrowBack } from 'react-icons/bi';
 
@@ -28,6 +31,7 @@ type Props = object;
 const Donate: React.FC<Props> = () => {
     const [informationForm, setInformationForm] = React.useState<DonationInformationForm | null>(null);
     const [paymentForm, setPaymentForm] = React.useState<DonationPaymentForm | null>(null);
+    const toast = useToast();
 
     const handleInformationSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -47,75 +51,98 @@ const Donate: React.FC<Props> = () => {
         const securityCode = Number(formData.get('securityCode'));
 
         setPaymentForm({ cardNumber, expiryDate, securityCode });
+        toast({
+            title: "Donation successful",
+            description: `Thank you for your donation of $${informationForm?.amount}`,
+            status: "success",
+            duration: 5000,
+            isClosable: true,
+        });
+        console.log(cardNumber, expiryDate, securityCode);
+        console.log(paymentForm);
     };
 
     return (
-        <Box p={8} maxW="800px" mx="auto">
-            {informationForm == null && (
-                <form onSubmit={handleInformationSubmit}>
-                    <Stack spacing={6}>
-                        <FormControl id="amount" isRequired>
-                            <FormLabel fontSize="lg">Donation Amount ($)</FormLabel>
-                            <Input name="amount" type="number" prefix="$" placeholder="1000" />
-                        </FormControl>
-
-                        <FormControl id="message">
-                            <FormLabel fontSize="lg">Donation Message</FormLabel>
-                            <Textarea name="message" placeholder="To those that serve our country..." />
-                        </FormControl>
-
-                        <Button
-                            bgColor="gray.500"
-                            color="white"
-                            size="lg"
-                            type="submit"
-                            width="full"
-                            fontSize="lg"
-                            fontWeight="bold"
-                        >
-                            Continue
-                        </Button>
-                    </Stack>
-                </form>
-            )}
-            {informationForm != null && (
-                <Box>
-                    <IconButton icon={<BiArrowBack />} aria-label="Back" onClick={() => setInformationForm(null)} />
-                    <Spacer h={8}/>
-                    <form onSubmit={handlePaymentSubmit}>
-                        <Stack spacing={6}>
-                            <FormControl id="cardNumber" isRequired>
-                                <FormLabel fontSize="lg">Card Number</FormLabel>
-                                <Input name="cardNumber" type="text" placeholder="1234 5678 9012 3456" />
-                            </FormControl>
-
-                            <HStack spacing={6}>
-                                <FormControl id="expiryDate" isRequired>
-                                    <FormLabel fontSize="lg">Expiry Date</FormLabel>
-                                    <Input name="expiryDate" type="text" placeholder="MM/YY" pattern="(?:0[1-9]|1[0-2])/[0-9]{2}" />
+        <>
+            <Heading as="h2" size="lg" textAlign="center" mb={8} color="black" fontFamily="body" mt={8}>
+                Donation
+            </Heading>
+            <Box
+                shadow="lg"
+                p={8}
+                mx="auto"
+                bgColor="white"
+                maxW="600px"
+                w="full"
+            >
+                <VStack spacing={8} align="center" w="full">
+                    {informationForm == null && (
+                        <form onSubmit={handleInformationSubmit} style={{width: '100%'}}>
+                            <Stack spacing={6}>
+                                <FormControl id="amount" isRequired>
+                                    <FormLabel fontSize="lg">Donation Amount ($)</FormLabel>
+                                    <Input name="amount" type="number" prefix="$" placeholder="1000" />
                                 </FormControl>
-                                <FormControl id="securityCode" isRequired>
-                                    <FormLabel fontSize="lg">Security Code</FormLabel>
-                                    <Input name="securityCode" type="number" placeholder="XXX" />
-                                </FormControl>
-                            </HStack>
 
-                            <Button
-                                bgColor="gray.500"
-                                color="white"
-                                size="lg"
-                                type="submit"
-                                width="full"
-                                fontSize="lg"
-                                fontWeight="bold"
-                            >
-                                Donate
-                            </Button>
-                        </Stack>
-                    </form>
-                </Box>
-            )}
-        </Box>
+                                <FormControl id="message">
+                                    <FormLabel fontSize="lg">Donation Message</FormLabel>
+                                    <Textarea name="message" placeholder="To those that serve our country..." />
+                                </FormControl>
+
+                                <Button
+                                    bgColor="gray.500"
+                                    color="white"
+                                    size="lg"
+                                    type="submit"
+                                    width="full"
+                                    fontSize="lg"
+                                    fontWeight="bold"
+                                >
+                                    Continue
+                                </Button>
+                            </Stack>
+                        </form>
+                    )}
+                    {informationForm != null && (
+                        <Box w="full">
+                            <IconButton icon={<BiArrowBack />} aria-label="Back" onClick={() => setInformationForm(null)} />
+                            <Spacer h={8}/>
+                            <form onSubmit={handlePaymentSubmit} style={{width: '100%'}}>
+                                <Stack spacing={6}>
+                                    <FormControl id="cardNumber" isRequired>
+                                        <FormLabel fontSize="lg">Card Number</FormLabel>
+                                        <Input name="cardNumber" type="text" placeholder="1234 5678 9012 3456" />
+                                    </FormControl>
+
+                                    <HStack spacing={6}>
+                                        <FormControl id="expiryDate" isRequired>
+                                            <FormLabel fontSize="lg">Expiry Date</FormLabel>
+                                            <Input name="expiryDate" type="text" placeholder="MM/YY" pattern="(?:0[1-9]|1[0-2])/[0-9]{2}" />
+                                        </FormControl>
+                                        <FormControl id="securityCode" isRequired>
+                                            <FormLabel fontSize="lg">Security Code</FormLabel>
+                                            <Input name="securityCode" type="number" placeholder="XXX" />
+                                        </FormControl>
+                                    </HStack>
+
+                                    <Button
+                                        bgColor="gray.500"
+                                        color="white"
+                                        size="lg"
+                                        type="submit"
+                                        width="full"
+                                        fontSize="lg"
+                                        fontWeight="bold"
+                                    >
+                                        Donate
+                                    </Button>
+                                </Stack>
+                            </form>
+                        </Box>
+                    )}
+                </VStack>
+            </Box>
+        </>
     );
 };
 
